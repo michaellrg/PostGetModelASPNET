a0e3b166640265a374577f6ede97599447169356
Michaell Reis Gasparini <michaellrg@gmail.com>
2016-09-26 00:24:17 -0300

    First PostGet Asp.Net

---
+++ PostGetModel/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using PostGetModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PostGetModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa { PessoaID = 1, Nome = "Michaell Gasparini", Twitter = "@michaellrg" };
+
+            //ViewData["key"] or ViewBag.Key
+            /* ViewBag.PessoaID = pessoa.PessoaID;
+             ViewBag.Nome = pessoa.Nome;
+             ViewBag.Twitter = pessoa.Twitter;*/
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public ActionResult Lista(Pessoa pessoa)
+        {
+
+            
+            return View(pessoa);
+        }
+    }
+}
